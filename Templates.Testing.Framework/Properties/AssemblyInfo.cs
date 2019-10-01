6ae6f4e5729072971dcc3f2efa74d7f73a97ff2a
JibbaJamba <nieto_b@yahoo.com>
2019-09-30 17:52:31 -0700

    Migrated project to GitHub as the primary location.  Updated NuGet packages to accomodate some of the latest and greatest out of NuGet.
    Updated Templates to no longer require having to update .dll references for custom packages, should work immediately after installing or updating the packages in your dependent projects.

--- Templates.Testing.Framework/Properties/AssemblyInfo.cs
+++ Templates.Testing.Framework/Properties/AssemblyInfo.cs
@@ -4,9 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Templates.Testing.Framework")]
 [assembly: AssemblyDescription("Framework for establishing Models and their relationships to their peer Models.")]
-[assembly: AssemblyCompany("Hyster Yale Group")]
 [assembly: AssemblyProduct("Templates.Testing.Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 
 [assembly: ComVisible(false)]
 
--- Templates.Testing/Properties/AssemblyInfo.cs
+++ Templates.Testing/Properties/AssemblyInfo.cs
@@ -4,9 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Templates.Testing")]
 [assembly: AssemblyDescription("Allows you to identify Models and their relationships to their peer Models, which it then generates a Repository Pattern suite that complies with the SOLID Principles and other Best Practices.  Automatically generates an effective set of Unit and Integration Tests.")]
-[assembly: AssemblyCompany("Hyster Yale Group")]
 [assembly: AssemblyProduct("Templates.Testing")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 
 [assembly: ComVisible(false)]
 
